5c443d61a1e581f1a859432f6f7099a28c4d0821
Pcamargoz <pedro01cesarsanto@gmail.com>
2025-03-17 20:12:47 -0300

    Colocando o codigo da conta com algumas coisas a mais como "IF" com a explição correta dentro do codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        // Criando um Scanner para entrada de dados;
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando e capturando os dados do usuário, e embaixo criando variaveis para armazenar o conteudo;
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        // IF para caso as informações da conta forem correspondente a da conta destinada;
+
+        if(nome.equalsIgnoreCase("PEDRO") && agencia.equals("1234") && numero == 100){
+
+            System.out.println("\nBem-vindo, Pedro! Sua conta foi criada com sucesso.");
+            System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$100.00 já está disponível.");
+
+        } else{
+            System.out.println("\nOs dados informados não correspondem aos esperados.");
+        }
+
+
+        // Fechando o Scanner;
+        scanner.close();
     }
 }
+
